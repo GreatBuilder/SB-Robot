42561593bfeba1ee305dc579de8f0d177331f9f2
GreatBuilder <great.builder@outlook.com>
2026-07-22 15:41:17 -0400

    Update closed-loop-velocity-control.ino
    - implemented single motor closed loop control that works

--- closed-loop-velocity-control/closed-loop-velocity-control.ino
+++ closed-loop-velocity-control/closed-loop-velocity-control.ino
@@ -10,32 +10,60 @@ void setup() {
   Serial.begin(115200);
   delay(2000);
 
-  Wire.begin();
-  Wire.setClock(100000);
+  SimpleFOCDebug::enable(&Serial);
+
+  // Encoder is connected to Wire1
+  Wire1.begin();
+  Wire1.setClock(100000);
 
   encoder.init(&Wire1);
+  motor.linkSensor(&encoder);
 
+  // Driver setup
   driver.voltage_power_supply = 11.0;
-  driver.init();
+
+  if (!driver.init()) {
+    Serial.println("Driver init failed");
+    while (1);
+  }
 
   motor.linkDriver(&driver);
 
-  motor.controller = MotionControlType::velocity_openloop;
+  // Closed-loop velocity control
+  motor.controller = MotionControlType::velocity;
+  motor.torque_controller = TorqueControlType::voltage;
+
+  // Motor safety limits
   motor.voltage_limit = 1.0;
+  motor.voltage_sensor_align = 1.0;
   motor.velocity_limit = 15.0;
-  motor.target = 5;
 
-  motor.init();
+  // Gentle velocity controller settings
+  motor.PID_velocity.P = 0.15;
+  motor.PID_velocity.I = 0.5;
+  motor.PID_velocity.D = 0.0;
 
-  Serial.println("Open-loop encoder test");
+  motor.LPF_velocity.Tf = 0.02;
+
+  if (!motor.init()) {
+    Serial.println("Motor init failed");
+    while (1);
+  }
+
+  Serial.println("Starting FOC alignment");
+
+  if (!motor.initFOC()) {
+    Serial.println("FOC alignment failed");
+    driver.disable();
+    while (1);
+  }
+
+  motor.target = 5.0;
+
+  Serial.println("Closed-loop motor ready");
 }
 
 void loop() {
   motor.loopFOC();
   motor.move();
-
-  encoder.update();
-
-  Serial.print("Angle: ");
-  Serial.println(encoder.getAngle(), 4);
 }
